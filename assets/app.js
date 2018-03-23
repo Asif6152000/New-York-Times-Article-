1e53158f2db9715d98a8fd80aaf0c16662ef0d1d
Chris Houck <chrishouck@Chriss-MacBook-Pro.local>
2018-03-23 14:12:45 -0400

    branch push

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,14 @@
+
 $("#searchButton").on("click", function(){
     // capture search input
     var q = $("#searchInput").val().trim();
+    console.log('q: ',q);
     // capture relevant parameters: TD
     // convert to relevant info to concatenate to the queryURL
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     queryURL += "?" + $.param({
         'api-key':'f619a2561fab45979fb395466716b7ba',
-        q
+        'q': q
     });
     // captures users input-ed number of search terms
     var resultsNumber = $("#resultNum").val();
@@ -15,9 +17,9 @@ $("#searchButton").on("click", function(){
         url: queryURL,
         method: 'GET'
     }).then(function(result){
-        for (var i =0; i<result.response.docs.length; i++){
+        console.log('result: ',result);
+        for (var i =0; i<resultsNumber.length; i++){
             console.log(result.response.docs[i].headline.main);
         }        
     });
-
 });
